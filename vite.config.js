1caa2447feaaa95f69d68816b219b12bd4e1ae4a
liamdermody <155569054+liamdermody@users.noreply.github.com>
2025-10-16 11:53:26 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()]
-  // Using a custom domain? Keep base default.
-  // If this is a project page (URL contains /REPO/), set: base: '/GOLDSHIRE/'
+  plugins: [react()],
+  base: '/',            // custom domain => keep at '/'
+  build: { outDir: 'dist' }
 })
