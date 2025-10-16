c7c2699da47728eeffce12ab93602219f095382b
liamdermody <liam.dermody@techflex.com>
2025-10-16 12:16:09 -0400

    Fix duplicate imports in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,3 @@
-// src/main.jsx
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(<App />)
-
-// src/main.jsx
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
